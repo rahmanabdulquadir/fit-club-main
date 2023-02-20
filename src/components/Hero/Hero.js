8b514ddcd871e8ede58c830ee57734a2ca0625f1
rahmanabdulquadir <rahmanaq777@gmail.comm>
2023-02-21 00:08:29 +0600

    header added

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Header from "../Header/Header";
 import "./Hero.css";
 
 const Hero = () => {
   return (
     <div className="hero">
       <div className="hero-l">
-        <h3>This is hero left side</h3>
+        <Header></Header>
       </div>
       <div className="hero-r">
         <h3>This is hero right side</h3>
