fabad750a36646e967bfab64cf2e46eec692a361
rahmanabdulquadir <rahmanaq777@gmail.comm>
2023-02-20 21:14:10 +0600

    project started

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-l">
+        <h3>This is hero left side</h3>
+      </div>
+      <div className="hero-r">
+        <h3>This is hero right side</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
