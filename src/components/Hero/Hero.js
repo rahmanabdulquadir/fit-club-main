64c9b5f78b76f717ba3ed22f782a7cc73c470b77
rahmanabdulquadir <rahmanaq777@gmail.comm>
2023-02-21 23:46:22 +0600

    hero title done

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,7 +10,27 @@ const Hero = () => {
 
         <div className="the-best-ad">
           <div></div>
-          <span>THE BEST FITNESS CLUB IN THE TOWN</span>
+          <span>the best fitness club in the town</span>
+        </div>
+        {/* Hero Header */}
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>Ideal Body</span>
+          </div>
+          <div>
+            <span>In here we will help you to shape and build your ideal body and live up your life to fullest</span>
+          </div>
+        </div>
+
+        {/* Figures */}
+        <div className="figures">
+          <div><span></span><span></span></div>
+          <div><span></span><span></span></div>
+          <div><span></span><span></span></div>
         </div>
       </div>
       <div className="hero-r">
