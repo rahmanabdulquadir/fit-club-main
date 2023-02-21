8c3828218a91ebfdc428e1f66aaaefbf9999adaf
rahmanabdulquadir <rahmanaq777@gmail.comm>
2023-02-21 23:17:37 +0600

    header section done

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,6 +7,11 @@ const Hero = () => {
     <div className="hero">
       <div className="hero-l">
         <Header></Header>
+
+        <div className="the-best-ad">
+          <div></div>
+          <span>THE BEST FITNESS CLUB IN THE TOWN</span>
+        </div>
       </div>
       <div className="hero-r">
         <h3>This is hero right side</h3>
