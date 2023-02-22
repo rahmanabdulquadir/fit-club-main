9cd9c248a28178cddf1f776681bb2ef8707a59a5
rahmanabdulquadir <rahmanaq777@gmail.comm>
2023-02-22 20:55:16 +0600

    left side of the hero section attached

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Header from "../Header/Header";
 import "./Hero.css";
+import hero_image from '../../assets/hero_image.png'
+import hero_image_back from '../../assets/hero_image_back.png'
+import Heart from '../../assets/heart.png'
+import Calories from '../../assets/calories.png'
 
 const Hero = () => {
   return (
@@ -22,19 +26,50 @@ const Hero = () => {
             <span>Ideal Body</span>
           </div>
           <div>
-            <span>In here we will help you to shape and build your ideal body and live up your life to fullest</span>
+            <span>
+              In here we will help you to shape and build your ideal body and
+              live up your life to fullest
+            </span>
           </div>
         </div>
 
         {/* Figures */}
         <div className="figures">
-          <div><span></span><span></span></div>
-          <div><span></span><span></span></div>
-          <div><span></span><span></span></div>
+          <div>
+            <span>+140</span>
+            <span>expert coachs</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+
+        {/* hero buttons */}
+
+        <div className="hero-buttons">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
         </div>
       </div>
+
+
+
+
+
       <div className="hero-r">
-        <h3>This is hero right side</h3>
+        <button className="btn">Join Now</button>
+
+
+        <div className="heart-rate">
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div>
       </div>
     </div>
   );
